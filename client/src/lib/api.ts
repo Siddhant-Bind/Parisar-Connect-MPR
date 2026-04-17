c0fb10a90272cc37cbece180d45b1d59f242d15f
Sorion <ix.sorion001@gmail.com>
2026-04-17 23:35:05 +0530

    minor deployment changes

--- client/src/lib/api.ts
+++ client/src/lib/api.ts
@@ -2,8 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL:
-    import.meta.env.VITE_API_BASE_URL ||
-    `http://${window.location.hostname}:8000/api/v1`,
+    import.meta.env.VITE_API_BASE_URL || `/api/v1`,
   headers: {
     "Content-Type": "application/json",
   },
